62cdc9c63334ed1be228fd0bc414cbd4ea7de3e4
narukami-nk <aokage.nk@gmail.com>
2021-12-12 18:00:15 +0900

    ユーザー管理機能j実装

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :password, format: {with: /[a-z\d]/, message:"is invalid. Include both letters and numbers" }
+  validates :nickname, presence: true
+  validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message:"is invalid. Input full-width characters "}
+  validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message:"is invalid. Input full-width characters "}
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message:"is invalid. Input full-width katakana characters "}
+  validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message:"is invalid. Input full-width katakana characters "}
+  validates :birth_day,       presence: true
+end
