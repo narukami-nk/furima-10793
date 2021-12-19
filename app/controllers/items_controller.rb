bca858e187cdef1a8bc2b2135ee35b1beaa5cecc
narukami-nk <aokage.nk@gmail.com>
2021-12-19 18:39:29 +0900

    出品画面遷移機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,19 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, only: [:new]
+
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  private
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
 end
